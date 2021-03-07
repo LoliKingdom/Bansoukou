781fe05eb999f1f450607b1e41f2ac8101258836
Rongmario <ethanmahmood@gmail.com>
2021-03-07 21:23:02 +0000

    Experimental branch

--- src/main/java/zone/rong/bansoukou/BansoukouTweaker.java
+++ src/main/java/zone/rong/bansoukou/BansoukouTweaker.java
@@ -1,9 +1,9 @@
 package zone.rong.bansoukou;
 
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
+import net.minecraft.launchwrapper.ITweaker;
 import net.minecraft.launchwrapper.Launch;
-import net.minecraftforge.common.ForgeVersion;
-import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import net.minecraft.launchwrapper.LaunchClassLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,18 +14,16 @@ import java.io.RandomAccessFile;
 import java.net.URI;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 import java.util.zip.ZipException;
 
-@IFMLLoadingPlugin.MCVersion(ForgeVersion.mcVersion)
-@IFMLLoadingPlugin.SortingIndex(Integer.MIN_VALUE + 100)
-@IFMLLoadingPlugin.Name("Bansoukou")
-public class BansoukouCoreMod implements IFMLLoadingPlugin {
+public class BansoukouTweaker implements ITweaker {
 
     public static final Logger LOGGER = LogManager.getLogger("Bansoukou");
 
-    public BansoukouCoreMod() throws IOException {
+    public BansoukouTweaker() throws IOException {
         LOGGER.info("Ikimasu!");
         File bansoukouRoot = new File(Launch.minecraftHome, "bansoukou");
         if (bansoukouRoot.mkdir()) {
@@ -45,26 +43,7 @@ public class BansoukouCoreMod implements IFMLLoadingPlugin {
         File mods = new File(Launch.minecraftHome, "mods");
         final Map<File, File> jars = new Object2ObjectOpenHashMap<>(patchRoot.length);
         for (File file : patchRoot) {
-            String fileName = file.getName().concat(".jar");
-            File pathFile = new File(mods, fileName);
-            Path path = pathFile.toPath();
-            if (Files.exists(path)) {
-                try (RandomAccessFile raf = new RandomAccessFile(pathFile, "r")) {
-                    int start = raf.readInt();
-                    if (start == 0x504B0304 || start == 0x504B0506 || start == 0x504B0708) {
-                        LOGGER.info("{} was found in the mods folder. Copying to modify the copied cache.", fileName);
-                    } else {
-                        throw LOGGER.throwing(new ZipException(fileName + " -> exists in the mods folder but isn't a valid jar file! Report to the pack author!"));
-                    }
-                }
-                File mockFile = new File(mods, fileName + ".disabled");
-                Files.copy(path, mockFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                File newFile = new File(mods, file.getName() + "-patched.jar");
-                Files.move(path, newFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                jars.put(file, newFile);
-            } else {
-                throw LOGGER.throwing(new FileNotFoundException(fileName + " -> does not exist in mods folder! Perhaps the mod's name has changed? Report to the pack author!"));
-            }
+            jars.put(file, patch(mods, file));
         }
         for (Map.Entry<File, File> entry : jars.entrySet()) {
             File root = entry.getKey();
@@ -115,6 +94,51 @@ public class BansoukouCoreMod implements IFMLLoadingPlugin {
         // ModContainerFactory.instance().registerContainerType(Type.getType(Mod.class), BansoukouModContainer.class);
     }
 
+    private File patch(File mods, File patchFile) throws IOException {
+        File modFile = new File(mods, patchFile.getName().concat(".jar"));
+        if (Files.exists(modFile.toPath()) || Files.exists((modFile = new File(mods, patchFile.getName().concat("-patched.jar"))).toPath())) {
+            try (RandomAccessFile raf = new RandomAccessFile(modFile, "r")) {
+                int start = raf.readInt();
+                if (start == 0x504B0304 || start == 0x504B0506 || start == 0x504B0708) {
+                    LOGGER.info("{} was found in the mods folder. Copying to modify the copied cache.", modFile.getName());
+                } else {
+                    throw LOGGER.throwing(new ZipException(modFile.getName() + " -> exists in the mods folder but isn't a valid jar file! Report to the pack author!"));
+                }
+            }
+            Path path = modFile.toPath();
+            File mockFile = new File(mods, patchFile.getName().concat(".jar") + ".disabled"); // Curseforge workaround
+            Files.copy(path, mockFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            File newFile = new File(mods, patchFile.getName() + "-patched.jar");
+            Files.move(path, newFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            return newFile;
+        } else {
+            throw LOGGER.throwing(new FileNotFoundException(modFile.getName() + " -> does not exist in mods folder! Perhaps the mod's name has changed? Report to the pack author!"));
+        }
+        /*
+        String fileName = file.getName().concat(".jar");
+        File pathFile = new File(mods, fileName);
+        Path path = pathFile.toPath();
+        if (Files.exists(path)) {
+            try (RandomAccessFile raf = new RandomAccessFile(pathFile, "r")) {
+                int start = raf.readInt();
+                if (start == 0x504B0304 || start == 0x504B0506 || start == 0x504B0708) {
+                    LOGGER.info("{} was found in the mods folder. Copying to modify the copied cache.", fileName);
+                } else {
+                    throw LOGGER.throwing(new ZipException(fileName + " -> exists in the mods folder but isn't a valid jar file! Report to the pack author!"));
+                }
+            }
+            File mockFile = new File(mods, fileName + ".disabled"); // Curseforge workaround
+            Files.copy(path, mockFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            File newFile = new File(mods, file.getName() + "-patched.jar");
+            Files.move(path, newFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            // jars.put(file, newFile);
+            return newFile;
+        } else {
+            throw LOGGER.throwing(new FileNotFoundException(fileName + " -> does not exist in mods folder! Perhaps the mod's name has changed? Report to the pack author!"));
+        }
+         */
+    }
+
     private void work(File f, Path currentPath) throws IOException {
         if (f.isDirectory()) {
             LOGGER.warn("Removing {} folder and anything under it...", currentPath);
@@ -140,26 +164,19 @@ public class BansoukouCoreMod implements IFMLLoadingPlugin {
     }
 
     @Override
-    public String[] getASMTransformerClass() {
+    public void acceptOptions(List<String> args, File gameDir, File assetsDir, String profile) { }
+
+    @Override
+    public void injectIntoClassLoader(LaunchClassLoader classLoader) { }
+
+    @Override
+    public String getLaunchTarget() {
         return null;
     }
 
     @Override
-    public String getModContainerClass() {
-        return null;
-    }
-
-    @Override
-    public String getSetupClass() {
-        return null;
-    }
-
-    @Override
-    public void injectData(Map<String, Object> data) { }
-
-    @Override
-    public String getAccessTransformerClass() {
-        return null;
+    public String[] getLaunchArguments() {
+        return new String[0];
     }
 
 }
